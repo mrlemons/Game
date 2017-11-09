374fb04d8884281e3513772c751b0f7baa26f773
mrlemons <32108038+mrlemons@users.noreply.github.com>
2017-11-09 10:40:51 -0500

    5

--- Game/src/game/model/Hero.java
+++ Game/src/game/model/Hero.java
@@ -8,6 +8,11 @@ public class Hero
 	private int vitality;
 	private int intelligence;
 	private int wisdom;
+	private int strMod;
+	private int dexMod;
+	private int vitMod;
+	private int intMod;
+	private int wisMod;
 	
 	Weapon weaponEquip;
 	Armor armorEquip;
@@ -33,6 +38,17 @@ public class Hero
 		weaponEquip.setWisMod(weapon.getWisMod());
 		
 		//Add weapon mods to our hero stats
+		strMod += weaponEquip.getStrMod();
+		dexMod += weaponEquip.getDexMod();
+		vitMod += weaponEquip.getVitMod();
+		intMod += weaponEquip.getVitMod();
+		wisMod += weaponEquip.getWisMod();
+		
+		strength += strMod;
+		dex += dexMod;
+		vitality += vitMod;
+		intelligence += intMod;
+		wisdom += wisMod;
 		
 		
 		
@@ -40,11 +56,34 @@ public class Hero
 	public void removeWeapon()
 	{
 		weaponEquip = null;
+		
+		strMod -= weaponEquip.getStrMod();
+		dexMod -= weaponEquip.getDexMod();
+		vitMod -= weaponEquip.getVitMod();
+		intMod -= weaponEquip.getVitMod();
+		wisMod -= weaponEquip.getWisMod();
+		
+		strength -= strMod;
+		dex -= dexMod;
+		vitality -= vitMod;
+		intelligence -= intMod;
+		wisdom -= wisMod;
 	}
 	
 	public void removeArmor()
 	{
 		armorEquip = null;
+		strMod -= armorEquip.getStrMod();
+		dexMod -= armorEquip.getDexMod();
+		vitMod -= armorEquip.getVitMod();
+		intMod -= armorEquip.getVitMod();
+		wisMod -= armorEquip.getWisMod();
+		
+		strength -= strMod;
+		dex -= dexMod;
+		vitality -= vitMod;
+		intelligence -= intMod;
+		wisdom -= wisMod;
 	}
 	
 	public void setArmorEquip(Armor armor)
@@ -57,8 +96,19 @@ public class Hero
 		armorEquip.setWisMod(armor.getWisMod());
 		
 		//Add armor mods to our hero stats
+		strMod += armorEquip.getStrMod();
+		dexMod += armorEquip.getDexMod();
+		vitMod += armorEquip.getVitMod();
+		intMod += armorEquip.getVitMod();
+		wisMod += armorEquip.getWisMod();
+		
+		strength += strMod;
+		dex += dexMod;
+		vitality += vitMod;
+		intelligence += intMod;
+		wisdom += wisMod;
 	}
-	
+
 	public Armor getArmorEquip()
 	{
 		return armorEquip;
@@ -127,7 +177,47 @@ public class Hero
 	{
 		this.wisdom = wisdom;
 	}
-
+	public int getStrMod()
+	{
+		return strMod;
+	}
+	public void setStrMod(int strMod)
+	{
+		this.strMod = strMod;
+	}
+	public int getDexMod()
+	{
+		return dexMod;
+	}
+	public void setDexMod(int dexMod)
+	{
+		this.dexMod = dexMod;
+	}
+	public int getVitMod()
+	{
+		return vitMod;
+	}
+	public void setVitMod(int vitMod)
+	{
+		this.vitMod = vitMod;
+	}
+	public int getIntMod()
+	{
+		return intMod;
+	}
+	public void setIntMod(int intMod)
+	{
+		this.intMod = intMod;
+	}
+	public int getWisMod()
+	{
+		return wisMod;
+	}
+	public void setWisMod(int wisMod)
+	{
+		this.wisMod = wisMod;
+	}
+	
 	
 	
 
